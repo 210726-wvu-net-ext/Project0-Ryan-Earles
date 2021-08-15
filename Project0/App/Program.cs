f06e5027f2a4070e90d2e8f03dc6edab269e3d8e
Rearles <53582049+Rearles@users.noreply.github.com>
2021-08-15 11:01:40 -0600

    Set up file organization structure (I think?)

---
+++ Project0/App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Lib;
+
+
+/// <summary>
+/// - add a new user 
+/// - ability to search user as admin
+/// - display details of a restaurant for user
+/// - add reviews to a restaurant as a user
+/// - view details of restaurants as a user
+/// - view reviews of restaurants as a user
+/// - calculate reviews’ average rating for each restaurant
+/// - search restaurant (by name, rating, zip code, etc.) 
+/// </summary>
+namespace App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+      
+        
+    }
+}
